1e2cef84331fa6b867e8b10b36c76933462acafb
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-01 16:42:00 -0700

    finish 05_sumAll assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    let small = Math.min(num1, num2);
+    let big = Math.max(num1, num2);
+    let ans = 0;
+    for (let i = small; i < big+1; i++) {
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
