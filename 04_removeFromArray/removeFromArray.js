b327c56560a9da318c7922a1af840e2100bf2a74
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-01 15:12:33 -0700

    finish 04_removeFromArray assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    return arr.filter(item => !itemsToRemove.includes(item));
 };
 
 // Do not edit below this line
