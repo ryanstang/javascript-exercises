25434a78e3fac69b585144e432913d481711f1cc
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-01 16:54:43 -0700

    finish 07_tempConversion assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let c = ((num - 32) * 5 / 9);
+  return c % 1 === 0 ? c : parseFloat(c.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let f =  (num * 9/5) + 32;
+  return f % 1 === 0 ? f : parseFloat(f.toFixed(1));
 };
 
 // Do not edit below this line
