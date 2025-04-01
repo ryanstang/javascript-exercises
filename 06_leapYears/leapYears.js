065f35597cea385bc3853dd312a34bb3b5920762
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-01 16:46:38 -0700

    finish 06_leapYears assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (num%4!=0 || (num%100===0 && num%400!=0)) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 // Do not edit below this line
