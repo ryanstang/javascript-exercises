f47a9dd43a244f9df2661e4a395c45c31edc9ca3
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-01 14:46:00 -0700

    finish 02_repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let ans = '';
+
+    for (let i=0; i < num; i++){
+        ans += word;
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
