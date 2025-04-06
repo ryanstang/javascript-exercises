d663418397b61bdfd15d37e75c4329190448cc80
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-05 18:41:05 -0700

    finish 10_fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if ( typeof num ==='string') {
+        num = parseInt(num);
+    }
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num === 0) {
+        return 0;
+    }
+
+    let num1 = 0;
+    let num2 = 1;
+    let temp;
+
+    for (let i = 1; i < num; i+=1) {
+        temp = num2;
+        num2 = num2 + num1;
+        num1 = temp;
+    }
+
+    return num2;
 };
 
 // Do not edit below this line
