17276a4308ee1b9fd1a758bcf53c37614c600e28
Ryan Tang <57160913+ryanstang@users.noreply.github.com>
2025-04-05 20:50:20 -0700

    finish 12_findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    let oldestPerson;
+    let oldestAge = 0;
 
+    people.forEach(person => {
+        let age;
+
+        if("yearOfDeath" in person){
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = currentYear - person.yearOfBirth;
+        }
+
+        if (oldestPerson === undefined || age > oldestAge) {
+            oldestPerson = person;
+            oldestAge = age;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
